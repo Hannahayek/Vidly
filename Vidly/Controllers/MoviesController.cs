5a3755793c1507eededeca95fd305fde5f23421d
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2018-08-22 13:32:13 +0800

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovie();
+            return View(movies);
+        }
+      
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+
+
+
+            return View(viewModel);
+        }
+
+
+    
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovie().SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+        private IEnumerable<Movie> GetMovie()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "SuperMan" },
+                new Movie { Id = 2, Name = "TomHanks" }
+            };
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
