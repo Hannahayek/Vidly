1f1a9b60bcd55792a5987b50056358121b4be9e3
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-08-27 18:14:42 +0800

    add Customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -29,13 +29,23 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
         }
+
+          [HttpPost]
+        public ActionResult Create(Customer customer)
+          {
+              _context.Customers.Add(customer);
+              _context.SaveChanges();
+
+              return RedirectToAction("Index","Customers");
+
+          }
 
 
 
@@ -60,5 +70,20 @@ namespace Vidly.Controllers
         //        new Customer { Id = 2, Name = "Mary Williams" }
         //    };
         //}
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                 Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+
+            };
+            
+            return View("CustomerForm",viewModel);
+        }
     }
 }
