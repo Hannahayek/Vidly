c9b425246914afeb7a3a899e5d83eb9d4f1007e3
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-09-11 13:34:54 +0800

    Get Customers/API

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -74,12 +74,12 @@ namespace Vidly.Controllers
           }
 
 
-
+        //node needed when call by api 
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
-            return View(customers);
+            return View();
         }
         public ActionResult Details(int id)
         {
